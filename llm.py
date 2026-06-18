846ca7ed9d5619d0964ab7fa3bfabb5a4554fb82
divyamt50 <divyamt50@gmail.com>
2026-06-18 21:54:14 +0530

    updated llm

--- llm.py
+++ llm.py
@@ -2,10 +2,19 @@ import os
 from openai import AsyncOpenAI
 from dotenv import load_dotenv
 load_dotenv()
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 
 GROQ_URL = os.getenv("GROQ_URL")
 
-async_client = AsyncOpenAI(
-    api_key=GROQ_URL,
-    base_url="https://api.groq.com/openai/v1"
-)
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    app.state.llm = AsyncOpenAI(
+        api_key=GROQ_URL,
+        base_url="https://api.groq.com/openai/v1"
+    )
+
+    yield
+    await app.state.llm.close()
+
+app = FastAPI(lifespan=lifespan)
